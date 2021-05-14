3809dc887f01b473675b2437f562ba02e510a00e
HenryGShoko <gshokx91@gmail.com>
2021-05-14 19:05:01 +0200

    plementing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
+
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://media-exp1.licdn.com/dms/image/C4D03AQExLyI0Dicm-A/profile-displayphoto-shrink_800_800/0/1601633489540?e=1626307200&v=beta&t=P4E4MZqC3D5xHRnnPbQI_XAhFYoHZMSeOdhb7GXRL78"/>
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
